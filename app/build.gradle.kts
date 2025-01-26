80121c0081c5852739a272382b81a291ed5e9f48
vinymd789k <vini777888999k@gmail.com>
2025-01-26 12:49:29 -0300

    Add new Font - Create New Models(Navigation Bar) - Create NavigationBottomBar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.7.6")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/portalparauapebasapp/ui/NewsScreen.kt
+++ app/src/main/java/com/example/portalparauapebasapp/ui/NewsScreen.kt
@@ -1,8 +1,9 @@
-package com.example.portalparauapebasapp.ui.theme.ui
+package com.example.portalparauapebasapp.ui
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+
 
 @Composable
 fun NewsScreen(modifier: Modifier = Modifier) {
